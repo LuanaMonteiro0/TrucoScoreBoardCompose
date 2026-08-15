5c8a36ce59db6dce34d6b016ddc8285624a255a6
Luana Monteiro <luanagiovanamonteiro@gmail.com>
2026-08-15 18:14:37 -0300

    feat: Creating basic screen elements

--- app/src/main/java/br/edu/ifsp/scl/sc3029972/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3029972/trucoscoreboardcompose/MainActivity.kt
@@ -1,15 +1,24 @@
 package br.edu.ifsp.scl.sc3029972.trucoscoreboardcompose
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3029972.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
@@ -20,9 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrucoScoreBoardComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    TrucoScoreBoardScreen(
+                        modifierParametro = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +39,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TrucoScoreBoardScreen( modifierParametro: Modifier = Modifier) {
+    Row(
+        modifier = modifierParametro.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Text(
+                text = "Equipe A",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Right,
+                textDecoration = TextDecoration.Underline
+            )
+
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Text(
+                text = "Equipe B",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Right,
+                textDecoration = TextDecoration.Underline
+            )
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TrucoScoreBoardScreenPreview() {
     TrucoScoreBoardComposeTheme {
-        Greeting("Android")
+        TrucoScoreBoardScreen(modifierParametro = Modifier.fillMaxSize())
     }
 }
+
