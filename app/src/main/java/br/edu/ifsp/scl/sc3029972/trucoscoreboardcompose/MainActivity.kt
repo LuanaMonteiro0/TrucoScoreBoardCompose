aab38f33932db7b3f98af697de07f845806f6e65
Luana Monteiro <luanagiovanamonteiro@gmail.com>
2026-08-15 18:26:43 -0300

    feat: added visual enhancement

--- app/src/main/java/br/edu/ifsp/scl/sc3029972/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3029972/trucoscoreboardcompose/MainActivity.kt
@@ -1,11 +1,9 @@
 package br.edu.ifsp.scl.sc3029972.trucoscoreboardcompose
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,11 +13,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3029972.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,10 +56,34 @@ fun TrucoScoreBoardScreen( modifierParametro: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth(),
                 textAlign = TextAlign.Right,
-                textDecoration = TextDecoration.Underline
+                textDecoration = TextDecoration.Underline,
+                fontSize = 20.sp
+            )
+
+            Text(
+                text = "0" ,
+                modifier = Modifier
+                        .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = 90.sp
+            )
+
+            Text(
+                text = "Pontos",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Left
             )
 
         }
+
+        VerticalDivider(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(horizontal = 4.dp),
+            color = Color.Gray,
+            thickness = 1.dp
+        )
 
         Column(
             modifier = Modifier
@@ -70,6 +96,21 @@ fun TrucoScoreBoardScreen( modifierParametro: Modifier = Modifier) {
                     .fillMaxWidth(),
                 textAlign = TextAlign.Right,
                 textDecoration = TextDecoration.Underline
+            )
+
+            Text(
+                text = "0" ,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = 90.sp
+            )
+
+            Text(
+                text = "Pontos",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Left
             )
 
         }
